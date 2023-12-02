1632e3a451a8e3ee7c06083fce4a451bde6a0fe6
Liam Pugh <liam.pugh.009@gmail.com>
2023-12-02 19:45:43 +0000

    day1-task1 complete

---
+++ days/day1.go
@@ -0,0 +1,75 @@
+package days
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func Run() {
+	inputs := convertInputToSlice()
+
+	count := 0
+	for _, input := range inputs {
+		count += findNumbers(input)
+	}
+
+	fmt.Println(count)
+}
+
+func convertInputToSlice() []string {
+	inputBytes, err := os.ReadFile("days/data/adventofcode.com_2023_day_1_input.txt")
+	if err != nil {
+		fmt.Println("could not read file")
+	}
+
+	var inputs []string
+
+	newWordCount := 0
+	for i := 0; i < len(inputBytes); i++ {
+		if string(inputBytes[i]) == "\n" {
+			inputs = append(inputs, string(inputBytes[newWordCount:i]))
+			newWordCount = i
+		}
+	}
+
+	return inputs
+}
+
+func findNumbers(input string) int {
+	start := 0
+	end := len(input) - 1
+	digitRegex := regexp.MustCompile(`\d`)
+	runEnd := true
+	runStart := true
+
+	firstNum := 0
+	lastNum := 0
+
+	for i := 0; i < len(input); i++ {
+		if runStart {
+			if ok := digitRegex.Match([]byte{input[start]}); ok {
+				firstNum, _ = strconv.Atoi(string(input[start]))
+				runStart = false
+			}
+		}
+		if runEnd {
+			if ok := digitRegex.Match([]byte{input[end]}); ok {
+				lastNum, _ = strconv.Atoi(string(input[end]))
+				runEnd = false
+			}
+		}
+
+		start++
+		end--
+	}
+
+	number := strconv.Itoa(firstNum) + strconv.Itoa(lastNum)
+	num, err := strconv.Atoi(number)
+	if err != nil {
+		panic(err)
+	}
+
+	return num
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/shutt90/advent-of-code-2023/days"
+)
+
+func main() {
+	days.Run()
+}
